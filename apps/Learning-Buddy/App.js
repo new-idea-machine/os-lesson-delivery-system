cd2e6baa35fade69d2d90c752c34c9e2d6b40f54
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-01 10:31:28 -0600

    Feat: setup testing dev env. fe/be integration

--- apps/Learning-Buddy/App.js
+++ apps/Learning-Buddy/App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [message, setMessage] = useState();
+
+
+  // TODO: Remove, for testing purposes
+  const getMessage = async () => {
+    try {
+      // Replace below URI with your own localhost, use ipConfig command in terminal to source your ipv4 address and replace up to the colon (:)
+      const response = await fetch('http://192.168.86.137:8000/message', {
+        // no-cors required to prevent conflict between emulator localhost/ server localhost
+        mode: 'no-cors' 
+      });
+      const json = await response.json();
+      console.log(json.data)
+      setMessage(json.data.message);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getMessage()
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <Text>{message}</Text>)}
     </View>
   );
 }
--- backend/app/api.py
+++ backend/app/api.py
@@ -8,6 +8,8 @@ origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
+# TEST: TODO remove 
+message = {"message": "sup brah"}
 
 
 app.add_middleware(
@@ -18,7 +20,12 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
+# DEV: TODO remove following two requests
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Hello World"}
+
+@app.get("/message", tags=["message"])
+async def get_message() -> dict:
+    return { "data": message }
+
