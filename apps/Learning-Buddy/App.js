0766d8480d8963d64d872f53fbe9bc18cc009fb7
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-03 11:42:28 -0600

    Removed misleading comment

--- apps/Learning-Buddy/App.js
+++ apps/Learning-Buddy/App.js
@@ -10,7 +10,6 @@ export default function App() {
   // TODO: Remove, for testing purposes
   const getMessage = async () => {
     try {
-      // Replace below URI with your own localhost, use ipConfig command in terminal to source your ipv4 address and replace up to the colon (:)
       const response = await fetch(`http://${ip}:8000/message`, {
         // no-cors required to prevent conflict between emulator localhost/ server localhost
         mode: 'no-cors',
