29331f26b147cee7d40940f0916c3d8ee606e6a1
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-05 13:17:17 -0600

    Feat: Stack Navigation

--- apps/Learning-Buddy/App.js
+++ apps/Learning-Buddy/App.js
@@ -1,44 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Constants from 'expo-constants';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AppStack } from './navigation/AppStack';
 
 export default function App() {
-  const ip = Constants.manifest.extra.IP;
-  const [isLoading, setLoading] = useState(true);
-  const [message, setMessage] = useState();
-
-  // TODO: Remove, for testing purposes
-  const getMessage = async () => {
-    try {
-      const response = await fetch(`http://${ip}:8000/message`, {
-        // no-cors required to prevent conflict between emulator localhost/ server localhost
-        mode: 'no-cors',
-      });
-      const json = await response.json();
-      setMessage(json.data.message);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getMessage();
-  }, []);
 
   return (
-    <View style={styles.container}>
-      {isLoading ? <Text>Still Loading...</Text> : <Text>{message}</Text>}
-    </View>
+    <SafeAreaProvider>
+    <AppStack />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
