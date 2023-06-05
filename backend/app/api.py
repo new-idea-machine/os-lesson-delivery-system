188d16e3ab37b1a21219e3700ccd23a260abc823
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-05 13:18:07 -0600

    Feat: OpenAI API integration proof of concept

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,23 +1,35 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+import requests
 
+load_dotenv()
 
+api_token = os.getenv("API-TOKEN")
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
-# TEST: TODO remove 
-message = {"message": "Success! Connected to Server"}
+origins = ["http://localhost:3000", "localhost:3000"]
 
+# TEST: TODO remove
+message = {"message": "Success! Connected to Server"}
+header = {"Authorization": f"Bearer {api_token}"}
+
+class Question(BaseModel):
+    id: int | None = None
+    name: str | None = None
+    question: str
+
+class Response(BaseModel):
+    response: dict
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 # DEV: TODO remove following two requests
@@ -27,5 +39,12 @@ async def read_root() -> dict:
 
 @app.get("/message", tags=["message"])
 async def get_message() -> dict:
-    return { "data": message }
+    return {"data": message}
 
+url = "https://api.openai.com/v1/models"
+
+@app.post("/questions", tags=["questions"])
+async def get_questions(question: Question) -> Response:
+    # print(question.question)
+    x = requests.get(url, headers=header)
+    return {"response": x.json()}
