dc7b955b0bd47439e2c1fce2ccc3b57801f8d536
George <gwmkwan@gmail.com>
2023-06-09 12:06:29 -0600

    feat: create them color file

---
+++ apps/Learning-Buddy/config/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  black: '#222222',
+  white: '#ffffff',
+  grey: '#6e6869',
+  red: '#E42E12',
+  blue: '#0D2FB8',
+  green: '#25CA34',
+  yellow: '#E4D112',
+  orange: '#f57c00'
+};
