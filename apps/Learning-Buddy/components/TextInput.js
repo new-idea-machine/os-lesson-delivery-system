dafb90088eb0fd7dbcc6e3dfe0e2c0089eaaf3e7
George <gwmkwan@gmail.com>
2023-06-09 14:53:26 -0600

    chore: rename file

---
+++ apps/Learning-Buddy/components/TextInput.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { colors } from '../config/colors';
+
+export const TextInput = ({ label, placeholder }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder={placeholder} style={styles.input} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20
+  },
+  label: {
+    marginBottom: 8,
+    color: colors.blue,
+    fontSize: 14,
+    fontWeight: '500'
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: colors.grey,
+    borderRadius: 14,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    flex: 1
+  }
+});
