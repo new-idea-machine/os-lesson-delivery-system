a86e51bbc7c204c56c011afa341192f153515eae
George <gwmkwan@gmail.com>
2023-06-09 14:52:43 -0600

    feat: create Button component

---
+++ apps/Learning-Buddy/components/Button.js
@@ -0,0 +1,31 @@
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { colors } from '../config/colors';
+
+export const Button = ({ title, onPress, style }) => {
+  console.log('INSIDE BUTTON');
+  return (
+    <TouchableOpacity
+      activeOpacity={0.6}
+      onPress={onPress}
+      style={[styles.container, style]}
+    >
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.blue,
+    paddingVertical: 20,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    width: '100%'
+  },
+  title: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+});
