7f48eb6d6a8cbd790f6ce2c29dffc474b1092806
George <gwmkwan@gmail.com>
2023-06-12 11:23:04 -0600

    feat: create checkbox

---
+++ apps/Learning-Buddy/components/Checkbox.js
@@ -0,0 +1,42 @@
+import { Image, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { colors } from '../config/colors';
+
+export default Checkbox = ({ checked, onCheck }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.6}
+      onPress={() => onCheck(!checked)}
+      style={styles.container}
+    >
+      {checked ? (
+        <View style={styles.innerContainer}>
+          <Image
+            style={styles.checkIcon}
+            source={require('./assets/check.png')}
+          />
+        </View>
+      ) : null}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: colors.grey,
+    borderWidth: 1,
+    borderRadius: 4,
+    width: 22,
+    height: 22
+  },
+  innerContainer: {
+    backgroundColor: colors.grey,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  checkIcon: {
+    width: 12,
+    height: 9
+  }
+});
