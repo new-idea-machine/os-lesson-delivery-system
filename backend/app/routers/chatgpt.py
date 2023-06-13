cd4be2196a7b0ac2300124324ce906785f0f9f08
Timothy H <huynh.tk80@gmail.com>
2023-06-13 06:52:52 -0600

    feat: endpoint shortanswer and mc

--- backend/app/routers/chatgpt.py
+++ backend/app/routers/chatgpt.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
 import openai
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/chat',
     tags=['chat']
 )
 
+class Request(BaseModel):
+    input: str
+    num_questions: int
 
 @router.get('/list')
 def list_questions():
@@ -13,15 +17,26 @@ def list_questions():
         "message": 'hello'
     }
 
-@router.get('/shortanswer')
-def list_questions(testinput: str, num_questions: int):
+@router.post('/shortanswer')
+def short_answer_questions(body: Request):
     completion = openai.Completion.create(
     model = "text-davinci-003",
     temperature = 0.8,
     max_tokens = 200,
-    prompt = "Use the following content to create a short answer quiz with " + str(num_questions) + " questions: " +  testinput
+    prompt = "Use only the following content to create a short answer quiz with {}  questions and return in a JSON format following the this outline {{ \"questions\":[]}}: {}".format(body.num_questions, body.input)
     )
     return{
-        "message": "Use the following content to create a short answer quiz with " + str(num_questions) + " questions and return in JSON format: " +  testinput,
-        "questions": completion
+        "results": completion.choices[0].text
+    }
+
+@router.post('/multiplechoice')
+def multiple_choice_questions(body: Request):
+    completion = openai.Completion.create(
+    model = "text-davinci-003",
+    temperature = 0.8,
+    max_tokens = 200,
+    prompt = "Use only the following content to create a multiple choice quiz with {}  questions and return in a JSON format following the this outline {{ \"questions\":[{{\"question\":\"\", \"options\":\"\", \"answers\":\"\"}}],}}: {}".format(num_questions,testinput)
+    )
+    return{
+        "results": completion.choices[0].text
     }
