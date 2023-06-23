88e840730a56c7bac3f6da8f0ba5ce2cf02947f3
George <gwmkwan@gmail.com>
2023-06-23 13:10:44 -0600

    chore: remove unused code

--- apps/Learning-Buddy/navigation/AppStack.jsx
+++ apps/Learning-Buddy/navigation/AppStack.jsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { QuestionForm } from '../screens/QuestionForm';
-import { SignInScreen } from '../screens/SignInScreen';
-import { SignUpScreen } from '../screens/SignUpScreen';
 import { TestScreen } from '../screens/TestScreen';
 
 const Stack = createStackNavigator();
@@ -12,14 +10,12 @@ export const AppStack = () => {
   // const navigation = useNavigation();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Sign up'>
-        <Stack.Screen
-          name='Question Form'
-          component={QuestionForm}
-        ></Stack.Screen>
-        <Stack.Screen name='Test Screen' component={TestScreen}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName='Question Form'>
+      <Stack.Screen
+        name='Question Form'
+        component={QuestionForm}
+      ></Stack.Screen>
+      <Stack.Screen name='Test Screen' component={TestScreen}></Stack.Screen>
+    </Stack.Navigator>
   );
 };
