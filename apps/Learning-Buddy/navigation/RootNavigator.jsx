b40bf848da9932f938400ddcc8cf8d739d291600
George <gwmkwan@gmail.com>
2023-06-23 10:19:38 -0600

    chore: set user variable with useState

--- apps/Learning-Buddy/navigation/RootNavigator.jsx
+++ apps/Learning-Buddy/navigation/RootNavigator.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthStack } from './AuthStack';
 import { AppStack } from './AppStack';
 
 export const RootNavigator = () => {
+  const { user } = useState();
+  // This will be replace with Auth Provider
   return (
     <NavigationContainer>
-      {/* {user ? <AppStack /> : <AuthStack />} */}
-      <AuthStack />
+      {user ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
 };
