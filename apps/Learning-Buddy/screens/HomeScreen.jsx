2b5aa5e51bb45e717e58f166ca7476a5b7100424
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-24 20:52:45 -0600

    New home screen

--- apps/Learning-Buddy/screens/HomeScreen.jsx
+++ apps/Learning-Buddy/screens/HomeScreen.jsx
@@ -1,5 +1,44 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
 import React from 'react';
+import BigButton from '../components/BigButton';
 
-export const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+export const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={localStyles.viewStyle}>
+      <Image
+        style={localStyles.logoStyle}
+        source={require('../assets/Logo.png')}
+      />
+      <View style={{ marginTop: 135 }}>
+        <BigButton
+          navigation={navigation}
+          content={'log in'}
+          destination={'Log in'}
+        />
+      </View>
+      <View style={{ marginTop: 15 }}>
+        <BigButton
+          navigation={navigation}
+          content={'sign up'}
+          destination={'Sign up'}
+        />
+      </View>
+    </View>
+  );
 };
+
+const localStyles = StyleSheet.create({
+  logoStyle: {
+    width: 159,
+    height: 118,
+    alignSelf: 'center',
+    marginTop: 120
+  },
+  viewStyle: {
+    display: 'flex',
+    backgroundColor: '#ffffff',
+    paddingTop: 50,
+    paddingHorizontal: 10,
+    height: '100%'
+  }
+});
