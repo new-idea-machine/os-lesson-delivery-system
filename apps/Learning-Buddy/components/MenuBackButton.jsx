5bcbd1f9681a7c7e3c6bd4f5e6adbf3975bbf701
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-24 22:54:21 -0600

    Added ripple effect to back button

--- apps/Learning-Buddy/components/MenuBackButton.jsx
+++ apps/Learning-Buddy/components/MenuBackButton.jsx
@@ -13,7 +13,7 @@ const MenuBackButton = ({navigation}) => {
 
   return (
     <View style={localStyles.viewStyle}>
-      <Pressable style={{flexDirection:'row'}} onPress={goBack}>
+      <Pressable style={{flexDirection:'row'}} onPress={goBack} android_ripple={{color: '#3CC982', borderless:true, radius:100}}>
             <Image
               style={styles.tinyLogo}
               source={require('../assets/baseline_chevron_left_black_18dp.png')
