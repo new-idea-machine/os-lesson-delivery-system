b0c3de3584b6a9fb2975008a4f7c238ca892fe86
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-06-24 20:52:35 -0600

    Appstack shows new screen names, initial route is now home

--- apps/Learning-Buddy/navigation/AppStack.jsx
+++ apps/Learning-Buddy/navigation/AppStack.jsx
@@ -2,25 +2,28 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { QuestionForm } from '../screens/QuestionForm';
-import { SignInScreen } from '../screens/SignInScreen';
+import { LoginScreen } from '../screens/LoginScreen';
 import { SignUpScreen } from '../screens/SignUpScreen';
 import { TestScreen } from '../screens/TestScreen';
+import {HomeScreen} from '../screens/HomeScreen'
 
 const Stack = createStackNavigator();
 
 export const AppStack = () => {
-  // const navigation = useNavigation();
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Sign in' screenOptions={{headerShown: false}}>
+      <Stack.Navigator
+        initialRouteName='Home Screen'
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen
           name='Question Form'
           component={QuestionForm}
         ></Stack.Screen>
-        <Stack.Screen name='Test Screen' component={TestScreen}></Stack.Screen>
-        <Stack.Screen name='Sign in' component={SignInScreen} />
+        <Stack.Screen name='Test Screen' component={TestScreen} />
+        <Stack.Screen name='Log in' component={LoginScreen} />
         <Stack.Screen name='Sign up' component={SignUpScreen} />
+        <Stack.Screen name ='Home Screen' component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
