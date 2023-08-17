2f9ba64f8b243f3d6793cc975ff244a410ccfdb7
George <gwmkwan@gmail.com>
2023-08-17 16:43:12 -0600

    chore: remove buttons and add a note

--- apps/Learning-Buddy/screens/HomeScreen.jsx
+++ apps/Learning-Buddy/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
+import { Paragraph } from 'react-native-paper';
 import BigButton from '../components/BigButton';
 import { colors } from '../config/colors';
 
@@ -11,29 +12,12 @@ export const HomeScreen = ({ navigation }) => {
   return (
     <View style={localStyles.container}>
       <Text>Hi {user.user_metadata.fullName}</Text>
+      <Paragraph>Please use the drawer menu from the top left corner</Paragraph>
       <BigButton
         buttonColor={colors.green}
         textColor={colors.black}
         content={'Create new quiz'}
         onPress={() => navigation.navigate('New Quiz Screen')}
-      />
-      <BigButton
-        buttonColor={colors.lightBlue}
-        textColor={colors.black}
-        content={'Get your questions!'}
-        onPress={() => navigation.navigate('Question Form')}
-      />
-      <BigButton
-        buttonColor={colors.green}
-        textColor={colors.black}
-        content={'Quiz Result'}
-        onPress={() => navigation.navigate('Quiz Result Screen')}
-      />
-      <BigButton
-        buttonColor={colors.lightBlue}
-        textColor={colors.black}
-        content={'Quiz Result Detail'}
-        onPress={() => navigation.navigate('Quiz Result Detail Screen')}
       />
       <BigButton
         buttonColor={colors.green}
