44311b3f371a3c522b115f11f446c4906c6ae03f
Timothy H <huynh.tk80@gmail.com>
2023-08-25 10:14:35 -0600

    fix: full_Name to fullName

--- supabaseScript.sql
+++ supabaseScript.sql
@@ -27,7 +27,7 @@ create function public.handle_new_user()
 returns trigger as $$
 begin
   insert into public.profiles (id, fullName, avatar_url, email)
-  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url', new.email);
+  values (new.id, new.raw_user_meta_data->>'fullName', new.raw_user_meta_data->>'avatar_url', new.email);
   return new;
 end;
 $$ language plpgsql security definer;
