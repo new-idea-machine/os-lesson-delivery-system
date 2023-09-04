88a246508f9a64866aaae608f31219ac28e6bf06
Siri <sirilakku01@gmail.com>
2023-09-04 17:51:47 -0600

    Code clean up added update, delete

--- backend/app/routers/quizQuestions.py
+++ backend/app/routers/quizQuestions.py
@@ -1,25 +1,3 @@
-# from fastapi import APIRouter, Depends
-# from pydantic import BaseModel
-# from sqlalchemy.orm import Session
-# from ..models import Questions
-# from ..database import Base
-
-
-# router = APIRouter(
-#     prefix='/quiz',
-#     tags=['quiz'])
-
-# # class Response(BaseModel):
-# #     response: dict
-
-# # class Request(BaseModel):
-# #     question_id: int
-
-# @router.get('/hello')
-# def testHello(): 
-#     return {
-#       "message": 'hello'
-#     }
 from fastapi import FastAPI, HTTPException, Depends,APIRouter
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
@@ -58,7 +36,6 @@ def testHello():
       "message": 'hello'
     }
 
-
 # # CRUD Operations
 # @router.post("/questions/", response_model=QuestionRead)
 # def create_question(question: QuestionCreate, db: Session = Depends(get_db)):
@@ -75,26 +52,26 @@ def read_question(question_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Question not found")
     return db_question
 
-# @router.put("/questions/{question_id}", response_model=QuestionRead)
-# def update_question(question_id: int, question: QuestionUpdate, db: Session = Depends(get_db)):
-#     db_question = db.query(Questions).filter(Questions.id == question_id).first()
-#     if db_question is None:
-#         raise HTTPException(status_code=404, detail="Question not found")
+@router.put("/questions/{question_id}", response_model=QuestionRead)
+def update_question(question_id: int, question: QuestionUpdate, db: Session = Depends(get_db)):
+    db_question = db.query(Questions).filter(Questions.id == question_id).first()
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
 
-#     for field, value in question.dict().items():
-#         setattr(db_question, field, value)
+    for field, value in question.dict().items():
+        setattr(db_question, field, value)
 
-#     db.commit()
-#     db.refresh(db_question)
-#     return db_question
+    db.commit()
+    db.refresh(db_question)
+    return db_question
 
-# @router.delete("/questions/{question_id}", response_model=str)
-# def delete_question(question_id: int, db: Session = Depends(get_db)):
-#     db_question = db.query(Questions).filter(Questions.id == question_id).first()
-#     if db_question is None:
-#         raise HTTPException(status_code=404, detail="Question not found")
+@router.delete("/questions/{question_id}", response_model=str)
+def delete_question(question_id: int, db: Session = Depends(get_db)):
+    db_question = db.query(Questions).filter(Questions.id == question_id).first()
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
 
-#     db.delete(db_question)
-#     db.commit()
-#     return {"message": "Question deleted"}
+    db.delete(db_question)
+    db.commit()
+    return {"message": "Question deleted"}
 
