2700034e5d4b0303923fc95cd7a895dea034dfe3
George <gwmkwan@gmail.com>
2023-09-07 15:16:47 -0600

    chore: clean up screen

--- apps/Learning-Buddy/screens/SaveDocumentsScreen.jsx
+++ apps/Learning-Buddy/screens/SaveDocumentsScreen.jsx
@@ -1,33 +1,44 @@
 import * as React from 'react';
-import { Button } from 'react-native-paper';
-import { View, Text, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { colors } from '../config/colors';
 
 export const SaveDocumentsScreen = ({ navigation }) => {
+  const insets = useSafeAreaInsets();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Save Documents</Text>
-      <Button
-        mode='contained'
-        onPress={() => navigation.navigate('Home Screen')}
-      >
-        Back To Home Screen
-      </Button>
+    <View
+      style={{
+        // Paddings to handle safe area
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right
+      }}
+    >
+      <ScrollView style={{ backgroundColor: colors.white }}>
+        <View style={localStyles.container}>
+          <View>
+            <Text style={localStyles.pageTitle}>Save Documents</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+const localStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 16,
-    backgroundColor: colors.white
+    marginHorizontal: 35
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20
+  pageTitle: {
+    marginTop: 20,
+    marginBottom: 10,
+    color: colors.black,
+    fontFamily: 'Poppins',
+    fontWeight: '900',
+    fontSize: 18,
+    lineHeight: 27,
+    alignSelf: 'flex-start'
   }
 });
