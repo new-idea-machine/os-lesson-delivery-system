0f4007bea20cc69a16bc0fef27c53fcc5fe5d7f1
George <gwmkwan@gmail.com>
2023-09-07 15:52:36 -0600

    feat: add card and styling

--- apps/Learning-Buddy/screens/SaveDocumentsScreen.jsx
+++ apps/Learning-Buddy/screens/SaveDocumentsScreen.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { Card, IconButton, Text } from 'react-native-paper';
 import { colors } from '../config/colors';
 
 export const SaveDocumentsScreen = ({ navigation }) => {
@@ -20,6 +21,52 @@ export const SaveDocumentsScreen = ({ navigation }) => {
           <View>
             <Text style={localStyles.pageTitle}>Save Documents</Text>
           </View>
+          <View style={localStyles.cardContainer}>
+            <Card mode='outlined' style={localStyles.card}>
+              <Card.Title
+                title='filename1.txt'
+                subtitle='# of days left'
+                titleStyle={localStyles.title}
+                subtitleStyle={localStyles.subtitle}
+                right={() => (
+                  <IconButton icon='dots-vertical' onPress={() => {}} />
+                )}
+              />
+            </Card>
+            <Card mode='outlined' style={localStyles.card}>
+              <Card.Title
+                title='filename2.txt'
+                subtitle='# of days left'
+                titleStyle={localStyles.title}
+                subtitleStyle={localStyles.subtitle}
+                right={() => (
+                  <IconButton icon='dots-vertical' onPress={() => {}} />
+                )}
+              />
+            </Card>
+            <Card mode='outlined' style={localStyles.card}>
+              <Card.Title
+                title='filename3.txt'
+                subtitle='# of days left'
+                titleStyle={localStyles.title}
+                subtitleStyle={localStyles.subtitle}
+                right={() => (
+                  <IconButton icon='dots-vertical' onPress={() => {}} />
+                )}
+              />
+            </Card>
+            <Card mode='outlined' style={localStyles.card}>
+              <Card.Title
+                title='filename4.txt'
+                subtitle='# of days left'
+                titleStyle={localStyles.title}
+                subtitleStyle={localStyles.subtitle}
+                right={() => (
+                  <IconButton icon='dots-vertical' onPress={() => {}} />
+                )}
+              />
+            </Card>
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -27,6 +74,12 @@ export const SaveDocumentsScreen = ({ navigation }) => {
 };
 
 const localStyles = StyleSheet.create({
+  fontStyle: {
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    // letterSpacing: 2,
+    fontWeight: '400'
+  },
   container: {
     flex: 1,
     marginHorizontal: 35
@@ -40,5 +93,25 @@ const localStyles = StyleSheet.create({
     fontSize: 18,
     lineHeight: 27,
     alignSelf: 'flex-start'
+  },
+  cardContainer: {
+    marginVertical: 20,
+    marginHorizontal: 15
+  },
+  card: {
+    marginVertical: 10,
+    backgroundColor: colors.white
+  },
+  title: {
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    lineHeight: 18,
+    fontWeight: '400'
+  },
+  subtitle: {
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    fontWeight: '400',
+    color: colors.grey
   }
 });
