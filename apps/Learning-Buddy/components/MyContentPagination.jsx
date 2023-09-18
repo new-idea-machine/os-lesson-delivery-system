8aac2eb724d7c5ba0c48b218046dd154d2de7629
Rashad Bayram <BRashad@users.noreply.github.com>
2023-09-18 11:00:21 -0600

    my content pagination buttons

---
+++ apps/Learning-Buddy/components/MyContentPagination.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+import { AntDesign } from '@expo/vector-icons';
+
+const MyContentBody = () => {
+  return (
+    <>
+      <View style={localStyles.container}>
+        <AntDesign name="left" size={24} color="#979797" />
+        <Text style={localStyles.text}>Last Page</Text>
+        <Text style={localStyles.text}>1</Text>
+        <Text style={localStyles.text}>/</Text>
+        <Text style={localStyles.text}>2</Text>
+        <Text style={localStyles.text}>Next Page</Text>
+        <AntDesign name="right" size={24} color="#979797" />
+      </View>
+    </>
+  );
+};
+
+const localStyles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    fontFamily: 'Poppins',
+    height:'7%'
+  },
+  text: {
+    color: '#979797',
+  }
+ 
+});
+
+export default MyContentBody;
