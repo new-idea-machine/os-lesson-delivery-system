537bf8512abe4d5840790ae3b2e908e21908de6d
Rashad Bayram <BRashad@users.noreply.github.com>
2023-09-18 11:01:23 -0600

    my content parent componnet

---
+++ apps/Learning-Buddy/components/MyContetTabs.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import MyContentButtons from './MyContentButtons';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const MyContentTabs = () => {
+  return (
+    <View style={[localStyles.container, localStyles.divider]}>
+      <Text style={localStyles.header}>My Content</Text>
+      <MyContentButtons />
+    </View>
+  );
+};
+
+const localStyles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    height:'30%'
+
+  },
+  divider: { marginVertical: 10 },
+  header: {
+    fontSize: 20,
+    color: 262626,
+    fontWeight: '800',
+  }
+});
+
+export default MyContentTabs;
