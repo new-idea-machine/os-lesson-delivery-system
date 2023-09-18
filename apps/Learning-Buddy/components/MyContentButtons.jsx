28f7d0c092ac57bf0e96716ec389d342a787f458
Rashad Bayram <BRashad@users.noreply.github.com>
2023-09-18 10:59:56 -0600

    my content tab buttons

---
+++ apps/Learning-Buddy/components/MyContentButtons.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+import { colors } from '../config/colors';
+
+const MyContentButtons = ({ ...props }) => {
+  let textColor = colors.black;
+  let borderColor = colors.black;
+  const MyContentButtonsNames = ['All Quizzes', 'Folders', 'Categories'];
+
+  return (
+    <View style={localStyles.container}>
+      {MyContentButtonsNames.map((buttonName, key) => (
+        <Button
+          labelStyle={[localStyles.fontStyle, { color: textColor }]}
+          mode='outlined'
+          style={[
+            localStyles.button,
+            { borderColor: borderColor },
+            localStyles.buttonContent
+          ]}
+          key={key}
+          {...props}
+        >
+          <Text style={localStyles.fontFamily}>{buttonName}</Text>
+        </Button>
+      ))}
+    </View>
+  );
+};
+
+const localStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'start'
+  },
+  button: {
+    margin: 8,
+    borderRadius: 5,
+    width: '37%'
+  },
+
+  md3FontStyles: {
+    lineHeight: 32
+  },
+  fontStyles: {
+    fontWeight: '800',
+    fontSize: 24
+  },
+  fontFamily: {
+    fontFamily: 'Poppins'
+  }
+});
+
+export default MyContentButtons;
