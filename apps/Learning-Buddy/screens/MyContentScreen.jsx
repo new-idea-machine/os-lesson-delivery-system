79911ad6cea21c08dc128bddd2f92839ffb50231
Rashad Bayram <BRashad@users.noreply.github.com>
2023-09-18 11:01:53 -0600

    my content screen page changes

--- apps/Learning-Buddy/screens/MyContentScreen.jsx
+++ apps/Learning-Buddy/screens/MyContentScreen.jsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import { Button } from 'react-native-paper';
-import { View, Text, StyleSheet } from 'react-native';
+import { Divider } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
 import { colors } from '../config/colors';
+import MyContentTabs from '../components/MyContetTabs';
+import MyContentBodyButtons  from '../components/MyContentBodyButtons';
+import MyContentPagination from '../components/MyContentPagination';
+
+
 
 export const MyContentScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>My Content</Text>
-      <Button
-        mode='contained'
-        onPress={() => navigation.navigate('Home Screen')}
-      >
-        Back To Home Screen
-      </Button>
+      <MyContentTabs />
+      <Divider />
+      <MyContentBodyButtons />
+      <MyContentPagination />
     </View>
   );
 };
@@ -20,14 +22,16 @@ export const MyContentScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     paddingHorizontal: 16,
-    backgroundColor: colors.white
+    backgroundColor: colors.white,
+    borderColor: 'blue',
+    borderWidth: 2
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 20
-  }
+  },
+  divider: { marginVertical: 10 }
 });
