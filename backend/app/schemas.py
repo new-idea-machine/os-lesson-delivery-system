388a03ea1b08b6576a1cabfabc889d4a08787e08
Rashad Bayram <BRashad@users.noreply.github.com>
2023-09-20 14:40:38 -0600

    create initial schemas for my content

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -33,3 +33,51 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+
+
+# Define a Pydantic schema for Quiz data
+class QuizBase(BaseModel):
+    title: str
+    description: str | None = None
+
+class QuizCreate(QuizBase):
+    pass
+
+class Quiz(QuizBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Define a Pydantic schema for Category data
+class CategoryBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+class CategoryCreate(CategoryBase):
+    pass
+
+class Category(CategoryBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+# Define a Pydantic schema for Folder data
+class FolderBase(BaseModel):
+    name: str
+    quizzes: List[Quiz] = []  # I assume store of Quiz objects in folders. Not sure if this is the best way to do it.
+
+class FolderCreate(FolderBase):
+    pass
+
+class Folder(FolderBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+    
+
