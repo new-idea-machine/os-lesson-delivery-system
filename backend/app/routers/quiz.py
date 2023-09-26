a96ce8cbac819b6b3ed9231ffcd82563bfdd16c8
Timothy H <huynh.tk80@gmail.com>
2023-09-26 14:42:27 -0600

    feat: endpoint get quizes

--- backend/app/routers/quiz.py
+++ backend/app/routers/quiz.py
@@ -6,6 +6,11 @@ from fastapi import  APIRouter, Request, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models
+from ..supabase import supabase
+from ..schemas import Quiz, QuizBase
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 router = APIRouter(
     prefix='/quiz',
@@ -28,10 +33,39 @@ async def get_quizes(request: Request, db: Session = Depends(get_db)) -> list:
     
     userId = data.user.id
     try:
-        response = db.query(models.Quiz).all()
-        print(response)
+        response = db.query(models.Quiz).filter(models.Quiz.user_id == userId).all()
+        pp.pprint(response)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred trying to access db: {e.message}")
 
-
+@router.get("/{quizId}", response_model=Quiz)
+async def get_quiz(quizId: int, request: Request, db: Session = Depends(get_db)):
+    # token = request.headers.get("authorization").replace("Bearer ", "")
+    # data: dict = supabase.auth.get_user(token)
+    
+    # if data is None:
+    #     raise HTTPException(status_code=401, detail="Invalid token")
+    
+    # userId = data.user.id
+    try:
+        response =  db.query(models.Quiz).filter(models.Quiz.id == quizId).first()
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred trying to access db: {e.message}")
+ 
+@router.post("/")
+async def create_quiz(request: Request, db: Session = Depends(get_db)) -> list:
+    token = request.headers.get("authorization").replace("Bearer ", "")
+    data: dict = supabase.auth.get_user(token)
+    
+    if data is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    userId = data.user.id
+    try:
+        response = db.query(models.Quiz).filter(models.Quiz.user_id == userId).all()
+        pp.pprint(response)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred trying to access db: {e.message}")
