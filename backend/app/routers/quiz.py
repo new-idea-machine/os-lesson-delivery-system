fde5e9282b35eb06f5d7ecc8639e5a122b18bef6
Timothy H <huynh.tk80@gmail.com>
2023-09-26 21:48:23 -0600

    cleanup quiz pprint

--- backend/app/routers/quiz.py
+++ backend/app/routers/quiz.py
@@ -8,9 +8,7 @@ from ..database import get_db
 from .. import models
 from ..supabase import supabase
 from ..schemas import Quiz, QuizBase
-import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
 
 router = APIRouter(
     prefix='/quiz',
@@ -34,7 +32,6 @@ async def get_quizes(request: Request, db: Session = Depends(get_db)) -> list:
     userId = data.user.id
     try:
         response = db.query(models.Quiz).filter(models.Quiz.user_id == userId).all()
-        pp.pprint(response)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred trying to access db: {e.message}")
@@ -55,7 +52,7 @@ async def get_quiz(quizId: int, request: Request, db: Session = Depends(get_db))
         raise HTTPException(status_code=500, detail=f"An error occurred trying to access db: {e.message}")
  
 @router.post("/")
-async def create_quiz(request: Request, db: Session = Depends(get_db)) -> list:
+async def create_quiz(request: Request, db: Session = Depends(get_db)):
     token = request.headers.get("authorization").replace("Bearer ", "")
     data: dict = supabase.auth.get_user(token)
     
@@ -65,7 +62,15 @@ async def create_quiz(request: Request, db: Session = Depends(get_db)) -> list:
     userId = data.user.id
     try:
         response = db.query(models.Quiz).filter(models.Quiz.user_id == userId).all()
-        pp.pprint(response)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred trying to access db: {e.message}")
+    
+
+# def create_quiz(db: Session, user: schemas.UserCreate):
+#     fake_hashed_password = user.password + "notreallyhashed"
+#     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
+#     return db_user
