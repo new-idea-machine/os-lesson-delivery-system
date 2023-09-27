55046be9c7b0ff41759c8ff64b80e92d618b7450
Timothy H <huynh.tk80@gmail.com>
2023-09-26 22:12:15 -0600

    feat: added save quiz on all genquestion endpoint

--- backend/app/routers/genQuestions.py
+++ backend/app/routers/genQuestions.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request,HTTPException
 import openai
 from pydantic import BaseModel
 import json
@@ -22,7 +22,14 @@ class GenQuizRequest(BaseModel):
 
 
 @router.post("/mc")
-async def get_questions(question: GenQuizRequest) -> Response:
+async def get_questions(question: GenQuizRequest, request: Request, db: Session = Depends(get_db)) -> Response:
+    token = request.headers.get("authorization").replace("Bearer ", "")
+    data: dict = supabase.auth.get_user(token)
+
+    if data is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    userId = data.user.id
     numQuestions = question.numQuestions
     text = question.text
     reqQuestion = f'Ask me {numQuestions} questions, multiple choice with four different potential answers, based only on this information: {text}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {{"topic":"", "questions": [{{"prompt": "", "qtype":1, "options": {{"Correct": "", "Incorrect": ["", "", ""]}}}},...]\}}'
@@ -33,10 +40,20 @@ async def get_questions(question: GenQuizRequest) -> Response:
         temperature=0,
         max_tokens=300,
     )
+    quiz = json.loads(response.choices[0].text)
+    create_quiz({**quiz, "user_id":userId, "quiz_type":0, "source_text":text}, db)
+
     return {"response": response}
 
 @router.post("/tf")
-async def get_questions(question: GenQuizRequest) -> Response:
+async def get_questions(question: GenQuizRequest, request: Request, db: Session = Depends(get_db)) -> Response:
+    token = request.headers.get("authorization").replace("Bearer ", "")
+    data: dict = supabase.auth.get_user(token)
+
+    if data is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    userId = data.user.id
     numQuestions = question.numQuestions
     text = question.text
     reqQuestion = f'Ask me {numQuestions} questions, true and false with two different potential answers, based only on this information: {text}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {{"topic":"", "questions": [{{"prompt": "", "qtype":2, "options": {{"Correct": "", "Incorrect": [""]}}}},...]\}}'
@@ -47,6 +64,9 @@ async def get_questions(question: GenQuizRequest) -> Response:
         temperature=0,
         max_tokens=300,
     )
+
+    quiz = json.loads(response.choices[0].text)
+    create_quiz({**quiz, "user_id":userId, "quiz_type":2, "source_text":text}, db)
     return {"response": response}
 
 
