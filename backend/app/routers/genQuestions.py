c5f8bebbaca6b45f15b0b13fbeed44582f009755
Timothy H <huynh.tk80@gmail.com>
2023-09-26 22:08:37 -0600

    feat: enable get userId from token

--- backend/app/routers/genQuestions.py
+++ backend/app/routers/genQuestions.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 import openai
 from pydantic import BaseModel
 import json
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from ..schemas import QuizCreate
 from .. import models
 from ..database import get_db
+from ..supabase import supabase
 
 router = APIRouter(
     prefix='/genquestions',
@@ -15,13 +16,13 @@ router = APIRouter(
 class Response(BaseModel):
     response: dict
 
-class Request(BaseModel):
+class GenQuizRequest(BaseModel):
     text: str
     numQuestions: int
 
 
 @router.post("/mc")
-async def get_questions(question: Request) -> Response:
+async def get_questions(question: GenQuizRequest) -> Response:
     numQuestions = question.numQuestions
     text = question.text
     reqQuestion = f'Ask me {numQuestions} questions, multiple choice with four different potential answers, based only on this information: {text}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {{"topic":"", "questions": [{{"prompt": "", "qtype":1, "options": {{"Correct": "", "Incorrect": ["", "", ""]}}}},...]\}}'
@@ -35,7 +36,7 @@ async def get_questions(question: Request) -> Response:
     return {"response": response}
 
 @router.post("/tf")
-async def get_questions(question: Request) -> Response:
+async def get_questions(question: GenQuizRequest) -> Response:
     numQuestions = question.numQuestions
     text = question.text
     reqQuestion = f'Ask me {numQuestions} questions, true and false with two different potential answers, based only on this information: {text}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {{"topic":"", "questions": [{{"prompt": "", "qtype":2, "options": {{"Correct": "", "Incorrect": [""]}}}},...]\}}'
@@ -49,8 +50,17 @@ async def get_questions(question: Request) -> Response:
     return {"response": response}
 
 
+
 @router.post("/mixed")
-async def get_questions(question: Request, db: Session = Depends(get_db)) -> Response:
+async def get_questions(question: GenQuizRequest, request: Request, db: Session = Depends(get_db)) -> Response:
+    token = request.headers.get("authorization").replace("Bearer ", "")
+    data: dict = supabase.auth.get_user(token)
+
+    if data is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    userId = data.user.id
+
     numQuestions = question.numQuestions
     text = question.text
     reqQuestion = f'Ask me {numQuestions} questions, they are a mix of multiple choice and true/false question, based only on this information: {text}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {{"quiz_name":"", "questions": [{{"prompt": "", "qtype":, "options": {{"Correct": "", "Incorrect": ["",...]}}}},...]\}} qtype is 1 if multiple choice and qtype is 2 for true and false questions'
@@ -62,7 +72,7 @@ async def get_questions(question: Request, db: Session = Depends(get_db)) -> Res
         max_tokens=300,
     )
     quiz = json.loads(response.choices[0].text)
-    create_quiz({**quiz, "user_id":"35c509ed-f3fa-4aa5-831c-d607bf9347c1", "quiz_type":1, "source_text":text}, db)
+    create_quiz({**quiz, "user_id":userId, "quiz_type":0, "source_text":text}, db)
     return {"response": response}
 
 
