5b7ade3fe8ab1260caad5bc4360d1e937f3f8829
Rashad Bayram <BRashad@users.noreply.github.com>
2023-10-05 07:37:33 -0600

    bug fxing and styles adjustment

--- apps/Learning-Buddy/components/MyContentData.jsx
+++ apps/Learning-Buddy/components/MyContentData.jsx
@@ -3,37 +3,52 @@ import { listAllQuizes } from '../util/quizAPI';
 import { useFocusEffect } from '@react-navigation/native';
 import { AuthContext } from '../providers/AuthProvider';
 import { View, StyleSheet, Text } from 'react-native';
-
+import { colors } from '../config/colors';
 import { Button } from 'react-native-paper';
 import { AntDesign } from '@expo/vector-icons';
 
 function MyContentData() {
   const [quizzes, setQuizzes] = useState([]);
-
+  let textColor = colors.black;
   const auth = useContext(AuthContext);
+  const MyContentButtonsNames = ['All Quizzes', 'Folders', 'Categories'];
+
+
+  const fetchData = React.useCallback(async () => {
+    if (!auth.session) {
+      return;
+    }
+
+    const data = await listAllQuizes(auth.session);
+
+    if (data) {
+      setQuizzes(data);
+    }
+  }, [auth.session]);
 
   useFocusEffect(
     React.useCallback(() => {
-      async function fetchData() {
-        if (!auth.session) {
-          return;
-        }
-
-        const data = await listAllQuizes(auth.session);
-        console.log('Fetched DATA', data);
-        if (data) {
-          setQuizzes(data);
-        }
-      }
-
       fetchData();
-    }, [auth.session])
+    }, [fetchData])
   );
 
   return (
     <>
       <View style={localStyles.titleContainer}>
         <Text style={localStyles.title}>All Quizzes</Text>
+      </View>
+      <View style={localStyles.containertab}>
+        {MyContentButtonsNames.map((buttonName) => (
+          <Button
+            labelStyle={[localStyles.fontStyletab, { color: textColor }]}
+            mode='outlined'
+            style={localStyles.buttontab}
+            key={buttonName}
+            onPress={fetchData}  // Call the fetchData function on button press
+          >
+            <Text style={localStyles.fontFamily}>{buttonName}</Text>
+          </Button>
+        ))}
       </View>
       <View style={localStyles.containerbodybuttons}>
         {quizzes.map((quiz) => (
@@ -102,6 +117,27 @@ const localStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
     marginHorizontal: 23
+  },
+  containertab: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'start'
+  },
+  buttontab: {
+    margin: 10,
+    borderRadius: 5,
+    width: 134
+  },
+
+  md3FontStyles: {
+    lineHeight: 32
+  },
+  fontStylestab: {
+    fontWeight: '800',
+    fontSize: 24
+  },
+  fontFamily: {
+    fontFamily: 'Poppins'
   }
 });
 
